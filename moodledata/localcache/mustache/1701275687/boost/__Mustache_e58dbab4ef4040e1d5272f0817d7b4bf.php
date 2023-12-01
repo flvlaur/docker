c893bffbb6b5f365b4937825907a6f25c29e6b85
Laurențiu-Flavius Istrate <laurentiu-flavius.istrate@students.plymouth.ac.uk>
2023-12-01 22:02:00 +0000

    1
    1

---
+++ moodledata/localcache/mustache/1701275687/boost/__Mustache_e58dbab4ef4040e1d5272f0817d7b4bf.php
@@ -0,0 +1,156 @@
+<?php
+
+class __Mustache_e58dbab4ef4040e1d5272f0817d7b4bf extends Mustache_Template
+{
+    private $lambdaHelper;
+
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $this->lambdaHelper = new Mustache_LambdaHelper($this->mustache, $context);
+        $buffer = '';
+
+        if ($parent = $this->mustache->loadPartial('core_form/element-template')) {
+            $context->pushBlockContext(array(
+                'element' => array($this, 'block62911360b1c5ce8abcd8dc35e0fa23a3'),
+            ));
+            $buffer .= $parent->renderInternal($context, $indent);
+            $context->popBlockContext();
+        }
+
+        return $buffer;
+    }
+
+    private function sectionCc48dcea9d2838da4d0362b228628420(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = 'is-invalid';
+            $result = (string) call_user_func($value, $source, $this->lambdaHelper);
+            $buffer .= $result;
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= 'is-invalid';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+    private function sectionD1efc3013753532711daaf50cc0a8149(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = '
+                autofocus aria-describedby="{{element.iderror}}"
+            ';
+            $result = (string) call_user_func($value, $source, $this->lambdaHelper);
+            $buffer .= $result;
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= $indent . '                autofocus aria-describedby="';
+                $value = $this->resolveValue($context->findDot('element.iderror'), $context);
+                $buffer .= ($value === null ? '' : call_user_func($this->mustache->getEscape(), $value));
+                $buffer .= '"
+';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+    private function sectionC87198b06c7ce18b3aeed72afb34afb9(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = 'disabled';
+            $result = (string) call_user_func($value, $source, $this->lambdaHelper);
+            $buffer .= $result;
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= 'disabled';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+    private function sectionBe7c95d5cc9bbac4935cf6f9d5f73268(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = '
+            readonly {{#element.hardfrozen}}disabled{{/element.hardfrozen}}
+            ';
+            $result = (string) call_user_func($value, $source, $this->lambdaHelper);
+            $buffer .= $result;
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= $indent . '            readonly ';
+                $value = $context->findDot('element.hardfrozen');
+                $buffer .= $this->sectionC87198b06c7ce18b3aeed72afb34afb9($context, $indent, $value);
+                $buffer .= '
+';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+    public function block62911360b1c5ce8abcd8dc35e0fa23a3($context)
+    {
+        $indent = $buffer = '';
+        $buffer .= $indent . '        <textarea
+';
+        $buffer .= $indent . '            name="';
+        $value = $this->resolveValue($context->findDot('element.name'), $context);
+        $buffer .= ($value === null ? '' : call_user_func($this->mustache->getEscape(), $value));
+        $buffer .= '"
+';
+        $buffer .= $indent . '            id="';
+        $value = $this->resolveValue($context->findDot('element.id'), $context);
+        $buffer .= ($value === null ? '' : call_user_func($this->mustache->getEscape(), $value));
+        $buffer .= '"
+';
+        $buffer .= $indent . '            class="form-control ';
+        $value = $context->find('error');
+        $buffer .= $this->sectionCc48dcea9d2838da4d0362b228628420($context, $indent, $value);
+        $buffer .= '"
+';
+        $value = $context->find('error');
+        $buffer .= $this->sectionD1efc3013753532711daaf50cc0a8149($context, $indent, $value);
+        $buffer .= $indent . '            ';
+        $value = $this->resolveValue($context->findDot('element.attributes'), $context);
+        $buffer .= ($value === null ? '' : $value);
+        $buffer .= '
+';
+        $value = $context->findDot('element.frozen');
+        $buffer .= $this->sectionBe7c95d5cc9bbac4935cf6f9d5f73268($context, $indent, $value);
+        $buffer .= $indent . '        >';
+        $value = $this->resolveValue($context->findDot('element.value'), $context);
+        $buffer .= ($value === null ? '' : call_user_func($this->mustache->getEscape(), $value));
+        $buffer .= '</textarea>
+';
+    
+        return $buffer;
+    }
+}
